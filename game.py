382872394f5a8eb7ad440c6a384f24a73401b1fc
Alec Berlin <alec.berlin21@gmail.com>
2025-01-30 09:35:45 -0700

    DEL: Repeated phrase

--- game.py
+++ game.py
@@ -14,9 +14,6 @@ print('Your journey begins here...')
 # Ask for the player's name
 player_name = input("what is your name, adventurer?")
 
-# Concatenate strings to create a personalized message
-print("Welcome, " + player_name + "! Your journey begins now.")
-
 # Use an f-string to display the same message in a more readable way
 print(f"Welcome, {player_name}! Your journey begins now.")
 
